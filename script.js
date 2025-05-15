104c391d5c30de7bd2029c3518c20caf415dc99c
Miqatali <alimiqat786@gmail.com>
2025-05-15 12:16:45 +0530

    navbar-changes

--- script.js
+++ script.js
@@ -80,3 +80,12 @@ function handleSubmit(button) {
 
 
 // js for navbar button
+document.addEventListener('DOMContentLoaded', function() {
+        const hamburger = document.getElementById('hamburger');
+        const navLinks = document.getElementById('navLinks');
+        
+        hamburger.addEventListener('click', function() {
+            navLinks.classList.toggle('active');
+            hamburger.classList.toggle('active');
+        });
+    });
